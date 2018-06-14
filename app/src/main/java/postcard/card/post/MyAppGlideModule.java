68d0f76dfb96fd0279c48cc09c8a560089016787
ayanbabi90 <ayanbabi1>
2018-06-15 02:04:05 +0530

    1. Downgrade the firebase api app gradle as .downloadurl is not supported on updated api 2 Added ImageCompressor lib to compress images 3.

---
+++ app/src/main/java/postcard/card/post/MyAppGlideModule.java
@@ -0,0 +1,9 @@
+package postcard.card.post;
+
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
